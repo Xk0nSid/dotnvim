eb064b8d698267c3131bc427464318baf1752e22
Siddharth Yaduvanshi <siddharth_yadav@outlook.com>
2025-01-13 17:58:43 +0530

    Treat header files as c for syntax highlighting

--- init.lua
+++ init.lua
@@ -31,3 +31,10 @@ vim.api.nvim_create_autocmd('TermOpen', {
     vim.opt.relativenumber = false
   end,
 })
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.h",
+  callback = function()
+    vim.bo.filetype = "c"
+  end,
+})
